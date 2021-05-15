92b4fb6555ab417fd6d79d544184cc34369c5c7b
Huang Sheng Fu <justdoit30927@gmail.com>
2021-05-15 15:20:35 +0800

    Create new Project

---
+++ Birthday2021/JS/config.js
@@ -0,0 +1,12 @@
+/*設定*/
+const firebaseConfig = {
+    apiKey: "AIzaSyAiNI5HBqlIypW1FL2ski1wl7KlXvMN13o",
+    authDomain: "fydb-1eb45.firebaseapp.com",
+    databaseURL: "https://fydb-1eb45-default-rtdb.firebaseio.com",
+    projectId: "fydb-1eb45",
+    storageBucket: "fydb-1eb45.appspot.com",
+    messagingSenderId: "1036728452964",
+    appId: "1:1036728452964:web:57094b4af2adaea5969c71",
+    measurementId: "G-67ER8EYM3H"
+};
+firebase.initializeApp(firebaseConfig);
